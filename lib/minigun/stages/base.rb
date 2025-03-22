9023514820e4d6c480b76750351bee148e0440c7
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-03-22 23:31:08 +0900

    Fix specs

--- lib/minigun/stages/base.rb
+++ lib/minigun/stages/base.rb
@@ -39,7 +39,7 @@ module Minigun
           @pipeline.send_to_next_stage(self, item, queue)
         end
       end
-      
+
       # Send an item to a specific queue
       def emit_to_queue(queue, item)
         # Track emit count in fork context if we're in a fork
@@ -49,20 +49,20 @@ module Minigun
           Thread.current[:minigun_fork_context][:success_count] ||= 0
           Thread.current[:minigun_fork_context][:success_count] += 1
         end
-        
+
         # Send to specified queue
         @pipeline.send_to_next_stage(self, item, queue.to_sym)
       end
-      
+
       # Alias for emit_to_queue
       alias_method :enqueue, :emit_to_queue
 
       # Called when the stage is starting
       def on_start
         @logger.info "[Minigun:#{@job_id}][#{name}] Stage starting"
-        
+
         # Execute before hooks
-        hook_name = "before_#{name}".to_sym
+        hook_name = :"before_#{name}"
         if @task.class.respond_to?(:_minigun_hooks) && @task.class._minigun_hooks[hook_name].is_a?(Array)
           @task.class._minigun_hooks[hook_name].each do |hook|
             if hook_should_run?(hook)
@@ -75,9 +75,9 @@ module Minigun
       # Called when the stage is finishing
       def on_finish
         @logger.info "[Minigun:#{@job_id}][#{name}] Stage finished"
-        
+
         # Execute after hooks
-        hook_name = "after_#{name}".to_sym
+        hook_name = :"after_#{name}"
         if @task.class.respond_to?(:_minigun_hooks) && @task.class._minigun_hooks[hook_name].is_a?(Array)
           @task.class._minigun_hooks[hook_name].each do |hook|
             if hook_should_run?(hook)
@@ -91,9 +91,9 @@ module Minigun
       def on_error(error)
         @logger.error "[Minigun:#{@job_id}][#{name}] Error: #{error.message}"
         @logger.error error.backtrace.join("\n") if error.backtrace
-        
+
         # Execute error hooks
-        hook_name = "on_error_#{name}".to_sym
+        hook_name = :"on_error_#{name}"
         if @task.class.respond_to?(:_minigun_hooks) && @task.class._minigun_hooks[hook_name].is_a?(Array)
           @task.class._minigun_hooks[hook_name].each do |hook|
             if hook_should_run?(hook)
@@ -109,7 +109,7 @@ module Minigun
         return unless @task.respond_to?(:_minigun_hooks)
 
         # Register stage-specific hooks if task has them
-        [:before_stage, :after_stage, :on_stage_error].each do |hook_type|
+        %i[before_stage after_stage on_stage_error].each do |hook_type|
           stage_hook = :"#{hook_type}_#{@name.to_s.gsub(/\s+/, '_').downcase}"
           if @task._minigun_hooks[stage_hook]
             @hooks ||= {}
@@ -120,18 +120,19 @@ module Minigun
 
       def call_hooks(hook_type, *args)
         return unless @hooks && @hooks[hook_type]
-        
+
         @hooks[hook_type].each do |hook_config|
           next unless hook_should_run?(hook_config)
+
           @task.instance_exec(*args, &hook_config[:block]) if hook_config[:block]
         end
       end
-      
+
       # Check if a hook should run based on its config
       def hook_should_run?(hook)
         # No conditions means always run
         return true unless hook[:if] || hook[:unless]
-        
+
         # Check if conditions
         if_result = true
         if hook[:if]
@@ -144,7 +145,7 @@ module Minigun
             end
           end
         end
-        
+
         # Check unless conditions
         unless_result = true
         if hook[:unless]
@@ -157,7 +158,7 @@ module Minigun
             end
           end
         end
-        
+
         # Both conditions must be true
         if_result && unless_result
       end
