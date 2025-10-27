6fe10750af3498d4d682d9fc7e054325b4ed65d5
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-10-27 21:21:09 +0900

    Remove some old code

--- spec_old/simple_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-RSpec.describe 'Simple Test' do # rubocop:disable RSpec/DescribeClass
-  it 'passes' do
-    expect(1 + 1).to eq(2)
-  end
-end
